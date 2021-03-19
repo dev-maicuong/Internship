aa004e3400f927e8fe7ea432506f9d47e39f04c1
hoangmaicuong99 <65660716+hoangmaicuong99@users.noreply.github.com>
2021-03-19 15:58:13 +0700

    Initial commit

---
+++ Internship/Controllers/CircularsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Internship.Controllers
+{
+    public class CircularsController : Controller
+    {
+        // GET: Circulars
+        public ActionResult IndexCirculars()
+        {
+            return View();
+        }
+        public ActionResult DetailCompany()
+        {
+            return View();
+        }
+    }
+}
